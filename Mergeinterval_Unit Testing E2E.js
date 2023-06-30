e2896b9292126cb5b58f803bbf2ed1d5b4258842
Yusuf Shalakhuddin <89732217+yFaizuS@users.noreply.github.com>
2023-06-30 18:30:49 +0700

    Update E2E test to use SeleniumWebdriver

--- Mergeinterval_Unit Testing E2E.js
+++ Mergeinterval_Unit Testing E2E.js
@@ -1,4 +1,4 @@
-const Solution = require('./MergeIntervalSolution');
+const { Builder, By } = require('selenium-webdriver');
 const assert = require('assert');
 
 // User Inputs
@@ -36,23 +36,58 @@ const expectedOutputs = [
 // Simulate the E2E test scenario
 console.log('Starting E2E test...');
 
-// Initialize the solution class
-const solution = new Solution();
+(async () => {
+  // Create a Selenium WebDriver instance
+  const driver = await new Builder().forBrowser('chrome').build();
 
-// Process user inputs and validate outputs
-for (let i = 0; i < userInputs.length; i++) {
-  console.log(`Executing Test Case ${i + 1}`);
+  try {
+    // Load the HTML file
+    await driver.get('file://C:/Users/LEGION/OneDrive/Documents/Project front End/Quality-Assurance_MergeInterval_UnitTest/MergeInterval_FormTesting.html');
 
-  // Simulate user input
-  const intervals = userInputs[i];
+    // Initialize the solution class
+    const Solution = require('./MergeIntervalSolution');
+    const solution = new Solution();
 
-  // Execute the function
-  const merged = solution.merge(intervals);
+    // Process user inputs and validate outputs
+    for (let i = 0; i < userInputs.length; i++) {
+      console.log(`Executing Test Case ${i + 1}`);
 
-  // Validate the output
-  const expected = expectedOutputs[i];
-  assert.deepStrictEqual(merged, expected);
-  console.log(`Test Case ${i + 1} Passed\n`);
-}
+      // Simulate user input
+      const intervals = userInputs[i];
 
-console.log('All test cases executed successfully.');
+      // Enter the intervals in the form
+      for (let j = 0; j < intervals.length; j++) {
+        const interval = intervals[j];
+        const inputElements = await driver.findElements(By.css(`#intervalInputs input:nth-child(${j + 1})`));
+        await inputElements[0].sendKeys(interval[0].toString());
+        await inputElements[1].sendKeys(interval[1].toString());
+      }
+
+      // Trigger form submission
+      const form = await driver.findElement(By.id('intervalForm'));
+      await form.submit();
+
+      // Wait for the merged intervals to be displayed
+      await driver.sleep(1000);
+
+      // Get the merged intervals from the UI
+      const mergedList = await driver.findElement(By.id('mergedList'));
+      const mergedText = await mergedList.getText();
+      const merged = JSON.parse(`[${mergedText}]`);
+
+      // Validate the output
+      const expected = expectedOutputs[i];
+      assert.deepStrictEqual(merged, expected);
+      console.log(`Test Case ${i + 1} Passed\n`);
+
+      // Clear the form for the next test case
+      const clearButton = await driver.findElement(By.id('clearButton'));
+      await clearButton.click();
+    }
+
+    console.log('All test cases executed successfully.');
+  } finally {
+    // Quit the WebDriver instance
+    await driver.quit();
+  }
+})();
